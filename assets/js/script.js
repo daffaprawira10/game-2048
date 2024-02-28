f5790524b053a73ebc4088756a9cc4c7c0634845
ikuzoo <142366274+daffaprawira10@users.noreply.github.com>
2024-02-28 12:01:39 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
   var popupMenu = document.getElementById('popupMenu');
   var closePopupIcon = document.getElementById('closePopup');
+  var closeAboutGame = document.getElementById('closeAboutGame');
+  var aboutGame = document.getElementById('aboutGame');
 
   // Fungsi untuk menampilkan pop-up
   function showPopup() {
@@ -12,8 +14,20 @@ document.addEventListener('DOMContentLoaded', function () {
     popupMenu.style.display = 'none';
   }
 
+  function showAboutGame() {
+    aboutGame.style.display = 'block';
+  }
+
+  function hideAboutGame() {
+    aboutGame.style.display = 'none';
+  }
+
   // Event listener untuk tombol play pada halaman index
   document.getElementById('playButton').addEventListener('click', showPopup);
+  document
+    .getElementById('game-info__icon')
+    .addEventListener('click', showAboutGame);
   // Event listener untuk ikon tutup
   closePopupIcon.addEventListener('click', hidePopup);
+  closeAboutGame.addEventListener('click', hideAboutGame);
 });
